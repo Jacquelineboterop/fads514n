8064e9b0a9ee62bb1c2c622fe1dd8a37de72ad20
Jacqueline <proyectos@branta.com.co>
2021-06-23 18:47:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: ['Javascript', 'CSS', 'HTLM', 'React']
+      list: ['Javascript', 'CSS', 'HTML', 'React']
     }
   }
   render() {
@@ -14,7 +14,9 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map(element => (
+            <li>{element}</li>
+          ))}
         </ul>
       </div>
     );
